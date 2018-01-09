ce4d65541a2e376f46232858b76f01cec3dc8947
Oleksiy2003 <oleksiy.lopatskiy@gmail.com>
2018-01-09 21:15:58 +0200

    Add project

---
+++ Assets/Player/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+
+    public float jumpSpeed;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+    private Animator myAnim;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        myAnim = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        if (isGrounded)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0f)
+            {
+                myRigidbody.velocity = new Vector3(moveSpeed, myRigidbody.velocity.y, 0f);
+                transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0f)
+            {
+                transform.localScale = new Vector3(-0.4f, 0.4f, 0.4f);
+                myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y, 0f);
+            }
+            else
+            {
+                myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
+            }
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+
+                myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0f);
+            }
+            if (isGrounded)
+            {
+
+            }
+            myAnim.SetFloat("Speed", Mathf.Abs(myRigidbody.velocity.x));
+            myAnim.SetBool("Grounded", isGrounded);
+        }
+        
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Animator anim;
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    public float jumpSpeed;
+    private bool faceRight = true; 
+
+
+	
+	void Start () {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	 
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up*jumpSpeed);
+        }
+        float moveX = Input.GetAxis("Horizontal");
+        rb.MovePosition(rb.position + Vector2.right * moveX * moveSpeed * Time.deltaTime);
+
+       
+        if(moveX > 0 && !faceRight)
+        {
+            
+            flip();
+        }
+       
+        if (moveX < 0 && faceRight)
+        {
+            
+            flip();
+        }
+        if(moveX < 0|| moveX > 0)
+        {
+            anim.SetBool("Walk",true);
+        }
+        else
+        {
+            anim.SetBool("Walk",false);
+        }
+
+
+    }
+    void flip() {
+        faceRight = !faceRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1,transform.localScale.y,transform.localScale.z); 
+    }
+}
